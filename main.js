aaa1429ae02b4c726798e05377ed657f9a9663ff
Aleksey <aleksey.bestuzhev@gmail.com>
2016-12-01 07:44:19 +0300

    changes

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
-$(document).ready(function() {
-    $(".fancybox").fancybox();
+// $(document).ready(function() {
+//     $(".fancybox").fancybox();
+// });
+
+var link = document.querySelector(".popap-feedback-link");
+var popap = document.querySelector(".popap-feedback");
+var close = document.querySelector(".popap-feedback_btn-close");
+var name = document.querySelector("[name=name]");
+
+
+link.addEventListener("click", function (event) {
+    event.preventDefault();
+    popap.classList.add("popap-feedback-show");
+    name.focus();
 });
+
+close.addEventListener("click", function (event) {
+    event.preventDefault();
+    popap.classList.remove("popap-feedback-show");
+});
+
+
+windows.addEventListener('keydown', function (event) {
+    if (event.keyCode === 27) {
+        if (popap.classList.contains('popap-feedback-show')) {
+            popap.classList.remove('popap-feedback-show');
+        }
+    }
+})
